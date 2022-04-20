5b1a7f6e3a3af2bbbfcc813cee1b895346cf5b07
Aneeka <aneeka_shahnewaz@hotmail.com>
2022-04-20 12:54:41 -0400

    login page added

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  type: boolean = true;
+  isLogin = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  changeType() {
+    this.type = !this.type;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    if(!form.valid) return;
+    this.login(form);
+  }
+
+  login(form) {
+    this.isLogin = true;
+  }
+
+}
