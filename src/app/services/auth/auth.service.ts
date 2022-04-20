b8b537bc88f970573191c93c7efdfcef79770561
Aneeka <aneeka_shahnewaz@hotmail.com>
2022-04-20 14:11:22 -0400

    auth service added

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { StorageService } from '../storage/storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private storage: StorageService) { }
+
+  async login(email: string, password: string): Promise<any> {
+    // call login api
+    return await this.storage.setStorage('uid', 'ASKFNJSNddASDSADNSANDSDS');
+  }
+
+  async getId() {
+    return (await this.storage.getStorage('uid')).value;
+  }
+
+  register() {}
+
+  resetPassword() {}
+
+  logout() {}
+}
